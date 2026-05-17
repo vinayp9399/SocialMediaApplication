7bf7cb67fcf1d0deab45517a48fcf2ab7ee57c88
Vinay Phatarpekar <vinayp9399@gmail.com>
2026-05-17 15:44:26 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app  = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({ 
-  origin: ["http://localhost:3000", "https://your-live-frontend.com"], 
+  origin: ["http://localhost:3000", "https://social-media-application-wi9u.vercel.app"], 
   credentials: true 
 }));
 app.use(express.json());
